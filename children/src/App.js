522e8afdf279462b9d32793fe210f002c4ce9189
Dan <daniel.sohval@gmail.com>
2019-07-16 15:47:56 -0400

    added lecture code

--- children/src/App.js
+++ children/src/App.js
@@ -1,56 +1,75 @@
 import React, { Component, Children } from 'react';
 
-class Emoji extends Component {
-  state = {
-    total: 0,
-    current: 0,
-
-  }
-
-  componentDidMount() {
-    const { children } = this.props;
-    this.setState({ total: Children.count(children) });
-    this.interval = setInterval(this.showNext, 3000);
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.interval);
-  }
-
-  showNext = () => {
-    const { total, current } = this.state;
-    this.setState({
-      current: current + 1 === total? 0 : current + 1
-    });
-  };
-
-  render() {
-    const { children } = this.props;
-    return (
-      <div>
-        <div className="emoji">
-            {Children.toArray(children)[this.state.current]}
-        </div>
-        <h3>Total Children: { Children.count(this.props.children) }</h3>
-        {children}
-      </div>
-    )
-  }
-}
-
-const owls = ['(*^▽^*)','( ﾟ∀ ﾟ)','ɾ⚈▿⚈ɹ','(≧∇≦*)', '≧(´▽｀)≦']
 const punctuation = [':=)', ':-9', ';-(']
+const owls = ['(*^▽^*)','( ﾟ∀ ﾟ)','ɾ⚈▿⚈ɹ','(≧∇≦*)', '≧(´▽｀)≦'];
+
+class Emoji extends Component {
+	state = {
+		total : 0,
+		current : 0
+	}
+
+	componentDidMount() {
+		const {children} = this.props
+
+		this.setState({total: Children.count(children)})
+		this.interval= setInterval(this.showNext, 2000)
+	}
+
+	componentWillUnmount() {
+		clearInterval(this.interval)
+	}
+
+
+	showNext = () => {
+		const {total, current} = this.state;
+		this.setState({
+			current: current + 1 === total ? 0 : current + 1
+		})
+
+	}
+	render() {
+		const {children} = this.props
+		console.log("My children!!!", children)
+		return(
+			<div>
+				<div className='emoji'>
+					{Children.toArray(children)[this.state.current]}
+				</div>
+				<h3>Total Children: {Children.count(children)} </h3>
+			</div>
+		)
+	}
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Emoji>
-          {[...punctuation]}
-        </Emoji>
+    	<Emoji>
+    	{[...owls, ...punctuation]}
+    	</Emoji>
       </div>
     );
   }
 }
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- shouldUpdate/src/App.js
+++ shouldUpdate/src/App.js
@@ -1,33 +1,33 @@
 import React, { Component } from 'react';
 
+class FruitSaver extends Component {
+	shouldComponentUpdate(nextProps) {
+		const updateBool = this.props.name !== nextProps.name
+		console.log("SHOULD I UPDATE?", updateBool)
+		return updateBool
+	}
 
-class FruitSaver extends React.Component {
-  shouldComponentUpdate (nextProps) {
-    const updateBool = this.props.name !== nextProps.name
-    
-    console.log("SHOULD I UPDATE?", updateBool)
-    
-    return updateBool
-  }
 
-  render () {
-    return <Fruit {...this.props} />
-  }
+	render() {
+		
+		return <Fruit {...this.props} />
+	}
 }
+
 
 const Fruit = (props) => {
-  console.log('IS THE FRUIT BEING RE-RENDERED?', props)
-  const name = props.name
-  return <h1>{name}</h1>
+	console.log("IS THE FRUIT BEING RENDERED?", props)
+
+	const {name} = props;
+	return <h1>{name}</h1>
 }
 
 
-
-const App = class extends React.Component {
+const App = class extends Component {
   state = {
     count: 0
   }
-
+  
   increment = () => {
     this.setState({
       count: this.state.count + 1
@@ -38,9 +38,8 @@ const App = class extends React.Component {
     return (
       <div>
         <h3>{this.state.count}</h3>
-        <FruitSaver name='banana' />
+        <FruitSaver name={'banana'} />
         <button onClick={this.increment}>Click me</button>
-        
       </div>
     )
   }
